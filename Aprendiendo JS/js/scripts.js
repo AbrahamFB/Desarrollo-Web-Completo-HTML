04e9cbda6c3d93b19836edc326a8cb82263d1120
Abraham Flores Basilio <44345256+AbrahamFB@users.noreply.github.com>
2020-07-13 14:22:17 -0500

    Strings o Cadenas de Texto

---
+++ Aprendiendo JS/js/scripts.js
@@ -0,0 +1,56 @@
+/*var nombre = prompt("Cuál es tu nombre?");
+
+      document.getElementById("mensaje").innerHTML = `Bienvenido ${nombre}`;*/
+
+console.log("Hola!");
+
+console.table([1, 2, 3, 4, 5]);
+
+console.error("Existe un error!!");
+
+//console.clear(); /*Limpiar la consola*/
+
+console.warn("Cuidado");
+
+// En JavaScript existen 3 formas de crear variables
+
+/** VAR */
+// var nombre, edad;
+
+// nombreCliente = "Abraham"; //Notación camello
+// ApellidoCliente = "Flores"; // Pascal Case
+// edad_cliente = 21; //underscore
+
+// console.log(nombreCliente);
+// console.log(ApellidoCliente);
+// console.log(edad_cliente);
+
+//Comentar todo Ctrl + k + c
+
+/* LET* */
+//No puedes crear dos let con el mismo nombre
+// let nombre;
+// nombre = "Liz";
+
+// console.log(nombre);
+
+/** CONST */
+// Es una variable constante no puede inicialisar vacia y no cambia
+
+// const cliente = "Abraham";
+
+// console.log(cliente);
+
+// //Uso, recomendado para las etiquetas de HTML
+// const divMensaje = document.getElementById("mensaje");
+
+let nombre;
+nombre = "Abraham";
+
+console.log(nombre);
+
+let aprendiendo = "Aprendiendo",
+  tecnologia = "JavaScript";
+
+console.log(`${aprendiendo} ${tecnologia}`);
+console.log(aprendiendo + " " + tecnologia); //Concatenar
