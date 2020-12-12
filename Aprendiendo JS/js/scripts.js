e884e8b09ce310937868e7bcb5d102ae96d0f249
Abraham Flores Basilio <44345256+AbrahamFB@users.noreply.github.com>
2020-12-12 16:47:51 -0600

    add

--- Aprendiendo JS/js/scripts.js
+++ Aprendiendo JS/js/scripts.js
@@ -265,3 +265,294 @@
 //console.log(persona["hogar"]["pais"]); //Acceder a sus prioridades
 
 //169 Template Strings
+//
+//const nombre = "Abraham",
+//  trabajo = "Desarrollador Web";
+//
+//  console.log("Nombre: " + nombre + ", Trabajo: " + trabajo);
+//
+//  console.log(`Nombre: ${nombre}, Trabajo: ${trabajo}`);
+//
+//  const contenedorApp = document.querySelector("#mensaje");
+//
+//  let html = "<ul>" +
+//                  "<li> Nombre: " + nombre + "</li>" +
+//                  "<li> Trabajo: " + trabajo + "</li>" +
+//            "</ul>";
+//contenedorApp.innerHTML = html;
+//
+//let html2 = `<ul>
+//                  <p>Template String</p>
+//                  <li> Nombre:  ${nombre} </li>
+//                  <li> Trabajo: ${trabajo}</li>
+//            </ul>`;
+//
+//
+//contenedorApp.innerHTML = html2;
+
+// 170 FUNCIONES
+
+////Function declaration
+//function saludar() {
+//  console.log("Hola");
+//}
+//
+//saludar();
+//
+//// Diferencia, la declarada puede llamarse antes de declararla
+//
+//// Funcion expression
+//const suma = function () {
+//  console.log(1 + 3);
+//};
+//
+//suma();
+//
+//saludar1("Abraham", "Desarrollor Web");
+//saludar1("Gabriela", "Licenciada en Criminología");
+//saludar1("Roberto", "Ingeniero Industrial");
+//saludar1("Juan");
+//saludar1();
+//function saludar1(nombre = "Visitante", trabajo = "No sabemos") {
+//  console.log(`Hola ${nombre} \nTu trabajo es: ${trabajo}`);
+//}
+//
+//const suma1 = function (a, b) {
+//  console.log(a + b);
+//};
+//suma1(10, 30);
+//suma1(1, 20);
+//suma1(12, 2130);
+//
+//// IIFE  ----- Se invocan ellas mismas
+//
+//(function () {
+//  console.log("Aquí estoy");
+//})();
+//
+//(function (tecnologia) {
+//  console.log(`Aquí estoy aprendiendo ${tecnologia}`);
+//})("JavaScript");
+
+// 171 Métodos o funciones dentro de los objetos
+
+//// Métodos
+//const musica = {
+//  reproducir: function (cancion) {
+//    console.log(`Reproduciendo la canción: ${cancion}`);
+//  },
+//  pausar: function(){
+//        console.log("Pausado");
+//  }
+//};
+//
+//musica.reproducir("Llegaste Tú");
+//musica.reproducir("Error");
+//musica.reproducir("Rain November");
+//musica.pausar();
+//
+////Los métodos también pueden ir por fuera
+//musica.borrar = function(id){
+//      console.log(`Borrando la canción con el ID: ${id}`);
+//}
+//
+//musica.borrar(121);
+
+//// 172 Funciones que retornan valores y Arrow Functions
+//
+//const suma1 = (
+//  a = 0,
+//  b = 0 // function (a = 0, b = 0)
+//) => a + b;
+//
+//const multiplicar = (a) =>
+//  // function (a)
+//  a * 10;
+//
+//let total;
+//let resultadoSuma = suma1(10, 20);
+//total = multiplicar(resultadoSuma);
+//
+//console.log(total);
+//
+////Arrow Functions
+//
+//let viajando = (destino) => `Viajando a la Ciudad de ${destino}`;
+//
+//let viaje;
+//viaje = viajando("Francia");
+//
+//console.log(viaje);
+
+// 173 Object Constructor y la palabra this
+
+//// Obecjt Literal
+//
+//const persona = {
+//  nombre: "Abraham",
+//  edad: 67,
+//  anioNacimiento: function () {
+//    return new Date().getFullYear() - this.edad;
+//  },
+//};
+//
+//console.log(persona.anioNacimiento());
+//
+//function Tarea(nombre, urgencia) {
+//  this.nombre = nombre;
+//  this.urgencia = urgencia;
+//}
+//
+////Nueva forma
+//
+////class Tarea {
+////  constructor(nombre, urgencia) {
+////    this.nombre = nombre;
+////    this.urgencia = urgencia;
+////  }
+////}
+//
+////Crear nuevas tareas
+//
+//const tarea1 = new Tarea("Aprender JavaScript", "Urgente");
+//const tarea2 = new Tarea("Arreglar mi cuarto", "Medio");
+//const tarea3 = new Tarea("Comenzar mis cursos", "Alto");
+//
+//console.log(tarea1);
+//console.log(tarea2);
+//console.log(tarea3);
+
+////174 Fechas
+//
+//const diaHoy = new Date();
+//
+//let valor = diaHoy;
+//
+////Retorna el número del mes 0 = Enero
+//valor = diaHoy.getMonth();
+//console.log(valor);
+//
+////Retorna el número del dia 0 = Domingo
+//valor = diaHoy.getDay();
+//console.log(valor);
+//
+////Retorna el número del dia del mes
+//valor = diaHoy.getDate();
+//console.log(valor);
+//
+//
+////Retorna minutos de la hora
+//valor = diaHoy.getMinutes();
+//console.log(valor);
+//
+////Retorna la hora
+//valor = diaHoy.getHours();
+//console.log(valor);
+//
+////Retorna un timestap
+//valor = diaHoy.getTime();
+//console.log(valor);
+//
+////establecer el año
+//valor = diaHoy.setFullYear(1999);
+//
+////Retorna el número del año actual
+//valor = diaHoy.getFullYear();
+//console.log(valor);
+//
+//// new Date es igual a MES, DÍA y AÑO
+//
+//const unDia = new Date("03-16-1999");
+//
+//let valor1;
+//
+//valor1 = unDia.getDay();
+//
+//console.log(valor1);
+
+//// 175 CONDICIONALES - IF, IFELSE, ELSE
+//
+//// === es operador estricto
+//const puntaje = 1000;
+//
+//if (puntaje === 1000) {
+//  console.log("Si es igual");
+//} else {
+//  console.log("No es igual");
+//}
+//
+//if (puntaje !== 1000) {
+//  console.log("Si es igual");
+//} else {
+//  console.log("No es igual");
+//}
+//
+//const logueado = true;
+//
+//if (logueado) {
+//  console.log("Si estas logueado");
+//} else {
+//  console.log("Inicia sesión");
+//}
+//
+//const edad = 18;
+//
+//if (edad >= 18) {
+//  console.log("Eres mayor de edad");
+//} else {
+//  console.log("Eres menor de edad");
+//}
+//
+//let dinero = 500;
+//let totalcarrito = 600;
+//
+//if (dinero > totalcarrito) {
+//  console.log("Pago realizado");
+//} else {
+//  console.log("Fondos insuficientes");
+//}
+//
+//let tarjeta = true;
+//
+//if (dinero > totalcarrito) {
+//  console.log("Pago realizado");
+//} else if (tarjeta) {
+//  console.log("Pagaste con tarjeta");
+//} else {
+//  console.log("Fondos insuficientes");
+//}
+//
+//// 176 OPERADOR && Y || Y EL TERNARIO
+//
+//// OR || SE DEBE CUMPLIR AL MENOS UNA
+//if (dinero > totalcarrito || tarjeta) {
+//  console.log("Pago realizado con tarjeta");
+//} else {
+//  console.log("Fondos insuficientes");
+//}
+//
+//// AND && SE DEBEN CUMPLIR TODAS
+//if (dinero > totalcarrito && tarjeta) {
+//  console.log("Pago realizado con tarjeta");
+//} else {
+//  console.log("Fondos insuficientes");
+//}
+//
+//let hora = 8;
+//if (hora > 0 && hora <= 12) {
+//  console.log("Buenos días");
+//} else if (hora > 12 && hora <= 18) {
+//  console.log("Buenas tardes");
+//} else if (hora > 18 && hora <= 24) {
+//  console.log("Buenas noches");
+//} else {
+//  console.log("Horario no disponible");
+//}
+//
+//// Ternario
+//
+//let logueado1 = true;
+//
+//console.log(logueado1 ? "Si se logueó" : "No se logueó");
+
+
