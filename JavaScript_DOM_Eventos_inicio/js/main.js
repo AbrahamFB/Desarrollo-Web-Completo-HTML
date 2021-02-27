59e198aa5616c9666ffcb11b656784c606301a96
Abraham Flores Basilio <44345256+AbrahamFB@users.noreply.github.com>
2021-02-27 09:37:46 -0600

    add

---
+++ JavaScript_DOM_Eventos_inicio/js/main.js
@@ -0,0 +1,7 @@
+// querySelector - Retorna ya sea ningun o solo uno que concuerde con el selector
+const heading = document.querySelector(".header__texto h2"); //Retorna 0 o 1 Elemento
+console.log(heading);
+
+// querySelectorAll
+
+// querySelectorAll
